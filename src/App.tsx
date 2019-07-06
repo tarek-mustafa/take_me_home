8ba58e08c7bc2e1a56b08a08f9450aa19d5844af
Tarek Mostafa <tarekmostafag@gmail.com>
2019-07-06 16:10:12 +0200

    add upload buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+
+const Container = styled.div`
+  display:flex;
+`
+
 
 const App: React.FC = () => {
+
+const [file, setFile] = 
+React.useState<{file: File, imagePreviewUrl: any }>()
+
+ const  _handleSubmit = (e: any) => {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', file);
+  }
+  
+    const _handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) =>  {
+    e.preventDefault();
+  
+    let reader = new FileReader();
+    let file = e.target.files![0];
+  
+    reader.onloadend = () => {
+      setFile({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+  
+    reader.readAsDataURL(file)
+  }
+  
+
+  let $imagePreview = null;
+  if (file && file.imagePreviewUrl) {
+    $imagePreview = (<img alt="cat" style={{width: "200px"}} src={file.imagePreviewUrl} />);
+  } else {
+    $imagePreview = (<div >Please select an Image for Preview</div>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    
+      <Container >
+        <form onSubmit={(e)=>_handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>_handleImageChange(e)} 
+            />
+
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>_handleSubmit(e)}>Submit image
+            </button>
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </Container>
+  )
+
 }
 
 export default App;
